51e101324e089d1fe83dd968cb9524db08854657
Catherine <Catherine@CatherinedeMacBook-Air.local>
2016-11-19 11:22:04 +0800

    Include libaray

---
+++ sketch_DTH_sensor_sample.ino
@@ -0,0 +1,15 @@
+// 引入函式庫
+#include <ESP8266WiFi.h>
+#include <WiFiudp.h>
+#include <WiFiClient.h>
+#include "DHT.h"
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
